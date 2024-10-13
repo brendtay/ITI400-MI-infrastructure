6186e5da3de73396644253d3607392bad1f332bd
Cbentz <55037407+RusskiyMedved@users.noreply.github.com>
2024-10-12 22:02:51 -0400

    fixed auth.js exports

---
+++ server/middleware/auth.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config(); // Load environment variables
+
+//Middleware to authenticate JWT tokens.
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']; 
+    const token = authHeader && authHeader.split(' ')[1]; 
+
+    if (!token) {
+        return res.status(401).json({ error: 'Access denied. No token provided.' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: 'Invalid or expired token.' });
+        }
+        req.user = user;
+        next(); 
+    });
+};
+
+//Authorize users based on role
+
+const isRole = (requiredRole) => (req, res, next) => {
+    try {
+        // Check if the user's role matches the required role
+        if (req.user.role !== requiredRole) {
+            return res.status(403).json({ error: `Access denied. ${requiredRole} only.` });
+        }
+        next(); // Proceed to the next middleware or route handler
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred during role verification.' });
+    }
+};
+
+module.exports = {
+    authenticateToken,
+    isRole
+};
