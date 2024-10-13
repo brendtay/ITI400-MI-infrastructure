59d5a0a3c7b282d5b1b7d090bf1618c329c3827b
Cbentz <55037407+RusskiyMedved@users.noreply.github.com>
2024-10-12 21:07:01 -0400

    change from bcrypt to bcrypt js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { getAllUsers, getUserByEmail, createUser, isEmailTaken } = require('../db/queries');
 const { authenticateToken, isRole } = require('../middleware/auth');
