5b2f4e4cb481b08319a979b1ec57acba6e6db7d3
brendtay <bjt273657@gmail.com>
2024-10-20 16:55:17 -0400

    fixed formating on mobile pt2

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark fixed-top">
       <div className="container-fluid">
-        <Link className="navbar-brand p-4" to="/">MI-Infrastructure</Link> {/* Link to Home */}
+        <Link className="navbar-brand p-4" to="/">MI-Infrastructure</Link>
         <button 
           className="navbar-toggler" 
           type="button" 
@@ -19,16 +19,16 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active p-2" aria-current="page" to="/form">Report An Issue</Link> {/* Link to Report An Issue */}
+              <Link className="nav-link active p-2" aria-current="page" to="/form">Report An Issue</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link p-2" to="/about">About Us</Link> {/* Link to About Us */}
+              <Link className="nav-link p-2" to="/about">About Us</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link p-2" to="/resources">Resources</Link> {/* Link to Resources */}
+              <Link className="nav-link p-2" to="/resources">Resources</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link p-2" to="/login">Log In</Link> {/* Link to Contact */}
+              <Link className="nav-link p-2" to="/login">Log In</Link>
             </li>
           </ul>
         </div>
