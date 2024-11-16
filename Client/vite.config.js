47f9aed32fd49720bf6336a0411ef3dc379d8c79
Cbentz <55037407+RusskiyMedved@users.noreply.github.com>
2024-11-16 05:54:56 -0500

    undoing tests

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { config as loadEnv } from 'dotenv'
-
-// Load .env file
-loadEnv();
 
 // https://vitejs.dev/config/
 export default defineConfig({
