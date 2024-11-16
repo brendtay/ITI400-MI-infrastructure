6bfd88ec03a5f1b0a4e3451f9c60fa804c54caf0
Cbentz <55037407+RusskiyMedved@users.noreply.github.com>
2024-11-16 05:50:03 -0500

    update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { config as loadEnv } from 'dotenv'
+
+// Load .env file
+loadEnv();
 
 // https://vitejs.dev/config/
 export default defineConfig({
