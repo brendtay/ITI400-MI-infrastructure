6fd659030005dff596e13df0547e1053c985b486
Cbentz <55037407+RusskiyMedved@users.noreply.github.com>
2024-11-16 04:39:09 -0500

    ip location part 2 electric boogaloo

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './pagesCss/Home.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from '../components/Navbar';
